dfdf857e038e82472aaf00c05c02c0810ab0e881
hyejinss <77151845+hyejinss@users.noreply.github.com>
2021-11-20 18:43:16 +0900

    Add files via upload
    Implement color picker with JColorChooser.

--- MouseHandler.java
+++ MouseHandler.java
@@ -1,14 +1,11 @@
 package frame;
 
-import java.awt.Color;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-
-import javax.swing.JColorChooser;
 
 
 public class MouseHandler implements MouseListener, MouseMotionListener, ActionListener {
@@ -22,13 +19,22 @@ public class MouseHandler implements MouseListener, MouseMotionListener, ActionL
 	//함수가 호출되는 시점을 파악해야한다!
 	
 	@Override
-	public void mouseClicked(MouseEvent e) { //커서를 움직이지 않고 클릭만 했을때 
+	public void mouseClicked(MouseEvent e) { //마우스가 해당 컴포넌트를 클릭했을 때 발생
 		System.out.println("mouseClicked");
 	}
 	
 	@Override
-	public void mousePressed(MouseEvent e) { //커서가 눌러졌을 때
-
+	public void mouseEntered(MouseEvent e) { //마우스가 해당 컴포넌트 영역 안으로 나갈 때 발생
+		
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) { //마우스가 해당 컴포넌트 영역 밖으로 나갈 때 발생
+		
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) { //마우스 버튼을 누를때 발생
 		Point start_point = e.getPoint(); //커서 시작의 좌표를 지역변수 start_point에 넣는다
 		canvas.start_point = start_point; //지역변수 start_point의 X,Y 좌표를 canvas의 전역변수 start_point X,Y 자리에 각각 넣는다(두 변수 모두 Point클래스 타입이기 때문에)
 	
@@ -36,25 +42,22 @@ public class MouseHandler implements MouseListener, MouseMotionListener, ActionL
 	}
 	
 	@Override
-	public void mouseReleased(MouseEvent e) { //눌러진 커서가 떼어질 때
+	public void mouseReleased(MouseEvent e) { //눌러진 마우스를 놓을 때 발생
 		System.out.println("mouseReleased");
 	}
 
-
+	
 	public void setcanvas(DrawingCanvas _canvas) {
 		canvas = _canvas;
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		
-		
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) { //커서가 드래그되는 동안
+	public void mouseDragged(MouseEvent e) { //해당 컴포넌트 위에서 마우스가 눌러진 상태로 드래그 될 때 발생
 		if(canvas == null) { //canvas를 참조하여 사용되는 함수는 exception이 발생되지않도록 null값일 경우 함수를 사용하지않고 리턴한다
 			return;
 		}
@@ -67,20 +70,8 @@ public class MouseHandler implements MouseListener, MouseMotionListener, ActionL
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent e) { //커서가 움직이는 동안
+	public void mouseMoved(MouseEvent e) { //해당 컴포넌트 위에서 마우스가 움직일 때 발생
 		System.out.println("마우스 Moved Called");
 	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		
-		
-	}
-	
 }///end of class
--- PaintFrame.java
+++ PaintFrame.java
@@ -4,14 +4,37 @@ import java.awt.Color;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class PaintFrame extends JFrame {
 	
-	private Color color; 
+	static JLabel label = new JLabel("Hello");
 	
+	public PaintFrame() {
+		
+	}
+	
+	static class MenuActionListener implements ActionListener {
+		JColorChooser chooser = new JColorChooser();
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String actionCommend = e.getActionCommand();
+			if(actionCommend.equals("COLOR")) {
+				Color selectedColor = chooser.showDialog(null, "COLOR", Color.yellow);
+				if(selectedColor != null) {
+					label.setForeground(selectedColor);
+				} /// end of if
+			} /// end of if
+		} /// end of actionPerFormed
+	} /// end of MenuActionListener
 	
 	//static 밖에 선언된 전역변수들을 static인 main메소드에서 사용할 경우 동일하게 static으로 명시해야 한다
 	//main메소드 전에 선언된 변수들은 프로그램 실행 전부터 메모리에 세팅된다
@@ -34,28 +57,50 @@ public class PaintFrame extends JFrame {
 		canvas.setBackground(Color.white);
 		
 		MenuBar menuBar = new MenuBar(); //메뉴바 생성
-		Menu file = new Menu("FILE");
+		Menu file = new Menu("FILE"); //menu file
 		MenuItem file_new = new MenuItem("NEW");
 		MenuItem file_open = new MenuItem("OPEN");
 		MenuItem file_save = new MenuItem("SAVE");
 		MenuItem file_exit = new MenuItem("EXIT");
 		
-		jframe.setMenuBar(menuBar); //프레임에 메뉴바 셋팅
-		
-		//하단 메뉴 추가
-		menuBar.add(file);
+		//file 하단 메뉴 추가
+		menuBar.add(file); //menu file
 		file.add(file_new);
 		file.add(file_open);
 		file.add(file_save);
 		file.add(file_save);
+		file.addSeparator(); //분리선
 		file.add(file_exit);
+		
+		Menu edit = new Menu("EDIT"); //menu edit
+		MenuItem edit_color = new MenuItem("COLOR");
+		MenuItem edit_shape = new MenuItem("SHAPE");
+		MenuItem edit_image = new MenuItem("IMAGE");
+		
+		
+		//edit 하단 메뉴 추가
+		menuBar.add(edit); //menu edit
+		edit.add(edit_color);
+		edit.add(edit_shape);
+		edit.add(edit_image);
+		
+		edit_color.addActionListener(new MenuActionListener());
+		
+		jframe.setMenuBar(menuBar); //프레임에 메뉴바 세팅
+		
 		
 		MouseHandler mouseHandler = new MouseHandler();
 		
 		canvas.addMouseListener(mouseHandler);
 		canvas.addMouseMotionListener(mouseHandler); //canvas에 mouseMotion 부착
 		mouseHandler.setcanvas(canvas);
-
+		
+		/*
+		label.setHorizontalAlignment(SwingConstants.CENTER); //hello를 가운데 정렬
+		label.setFont(new Font("Ravie", Font.ITALIC,30)); //hello 글꼴 설정
+		jframe.add(label, BorderLayout.CENTER);
+		*/
+		
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE); //윈도우창 종료시 프로세스 자동 종료
 		
 
